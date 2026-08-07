8462a1a533338babd970cfbfd4b1cde3fb48010a
Francisco Menchaca Giorello <franmenchaca2004@gmail.com>
2026-08-07 15:23:57 -0300

    Creacion de Domain/Equipo

---
+++ Domain/Equipo.cs
@@ -0,0 +1,24 @@
+﻿namespace Domain;
+
+public class Equipo
+{
+    public static int IdStatic { get; set; }
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public string Descripcion { get; set; }
+    public DateTime FechaFundacion { get; set; }
+    public string Ciudad { get; set; }
+    public string Escudo { get; set; }
+
+    public Equipo(string Nombre, string Descripcion, DateTime FechaFundacion, string Ciudad, string Escudo)
+    {
+        this.Id = Equipo.IdStatic++;
+        this.Nombre = Nombre;
+        this.Descripcion = Descripcion;
+        this.FechaFundacion = FechaFundacion;
+        this.Ciudad = Ciudad;
+        this.Escudo = Escudo;
+    }
+    
+    
+}
